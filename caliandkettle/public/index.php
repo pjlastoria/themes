36771c04ca1d93f4f18b5ced1ef88c12eb01b7f2
pjlastoria <pjaylastoria@gmail.com>
2024-05-30 07:44:15 -0400

    Add files via upload

---
+++ caliandkettle/public/index.php
@@ -0,0 +1,571 @@
+<?php
+
+include('views/header.php');
+
+
+?>
+
+<section>
+    <nav>
+        <div class="logo-wrapper">
+            <img src="images/logo.png" alt="">
+        </div>
+
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Guide</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+
+        <div class="nav-btn-wrapper">
+            <button class="nav-btn">
+                Join Now
+            </button>
+        </div>
+    </nav>
+</section>
+
+<section id="hero">
+
+    <div class="hero-left">
+
+        <div class="text-wrapper">
+            <h1 class="hero-headline">
+                Increase Strength at Home With Little to No Equipment
+            </h1>
+
+            <div class="hero-cta">
+                <span class="register-btn">
+                    Register Now
+                    <img src="images/arrow-top-right.svg" id="cta-arrow" alt="">
+                </span>
+                <p class="cta-desc">
+                    Learn the tips and tricks how to get in great shape at home
+                    while saving money on a gym membership.
+                </p>
+            </div>
+        </div>
+
+        <div class="hero-left-img">
+            <svg>
+                <polygon points="100,0 0,200 200,200" style="fill:skyblue;" />
+            </svg>
+
+            <img src="images/fitness-hero-1.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="hero-right">
+
+        <div class="content-container">
+
+
+            <div class="hero-right-img">
+                <svg>
+                    <circle cx="150" cy="150" r="150" fill="#ED874E" />
+                </svg>
+
+                <img src="images/fitness-hero-2.png" alt="">
+            </div>
+
+            <div class="hero-items-wrapper">
+                <div class="hero-item-card">
+                    <div class="item-img-wrapper">
+                        <img src="images/hero-item-1.png" alt="">
+                    </div>
+
+                    <span class="item-name">
+                        Jump Rope
+                    </span>
+                </div>
+
+                <div class="hero-item-card">
+                    <div class="item-img-wrapper">
+                        <img src="images/hero-item-2.png" alt="">
+                    </div>
+                    <span class="item-name">
+                        Kettlebell
+                    </span>
+                </div>
+
+                <div class="hero-item-card">
+                    <div class="item-img-wrapper">
+                        <img src="images/hero-item-3.png" alt="">
+                    </div>
+                    <span class="item-name">
+                        Dumbbells
+                    </span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</section>
+
+<section id="under-hero">
+
+    <div class="hero-plans-wrapper">
+        <div class="plans-top">
+            <div class="plans-top-left">
+                <div class="plans-header">
+                    <img src="images/heart.svg" alt="">
+                    <h2>Fitness Plans and Nutrition</h2>
+                </div>
+                <p class="plans-desc">
+                    7 Steps to Creating Your Perfect Diet Plan for Maximum Performance
+                </p>
+                <span class="learn-more-btn">
+                    Learn More
+                    <img src="images/arrow-right.svg" id="cta-arrow" alt="">
+                </span>
+            </div>
+
+            <div class="plans-top-right">
+                <div class="svg-img-wrapper">
+                    <svg width="100" height="100">
+                        <rect width="100" height="100" style="fill:gold;" />
+                    </svg>
+                    <img src="images/fitness-hero-3.png" alt="">
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="under-hero-right">
+        <p class="under-hero-text">
+            Checkout This Weeks Deals
+        </p>
+        <div class="circle-text-wrapper">
+            <img src="images/circle-text.svg" alt="">
+            <svg width="150" height="150">
+                <circle cx="75" cy="75" r="40" fill="#000" />
+            </svg>
+            <img src="images/dumbbell.svg" alt="">
+        </div>
+    </div>
+
+</section>
+
+
+<section id="banners">
+
+    <div class="banner-left">
+        <div class="banner">
+            <img src="images/banner-1.jpg" alt="">
+            <div class="banner-bg">
+                <div class="banner-content">
+                    <p class="banner-text">
+                        Achieve Peak Conditioning
+                    </p>
+                    <button>
+                        Signup Today
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="banner-right">
+        <div class="banner">
+            <img src="images/banner-2.jpg" alt="">
+            <div class="banner-bg">
+                <div class="banner-content">
+                    <p class="banner-text">
+                        Our Trainers Will Help You
+                    </p>
+                    <button>
+                        Find A Trainer
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<section id="services">
+
+    <div class="services-wrapper">
+        <h1>Our Services</h1>
+
+        <div class="services">
+            <div class="service-card">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="images/strong-arm.svg" alt="">
+                    </div>
+                    <span class="card-title">Gain Strength</span>
+                    <p class="card-desc">
+                        Learn how to gain strength while losing
+                        fat from our very experienced personal trainers.
+                    </p>
+                    <span class="learn-more-btn">
+                        Learn More
+                        <img src="images/arrow-right.svg" id="cta-arrow" alt="">
+                    </span>
+                </div>
+            </div>
+
+            <div class="service-card">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="images/tomato.svg" alt="">
+                    </div>
+                    <span class="card-title">Increase Endurance</span>
+                    <p class="card-desc">
+                        Learn how to gain strength while losing
+                        fat from our very experienced personal trainers.
+                    </p>
+                    <span class="learn-more-btn">
+                        Learn More
+                        <img src="images/arrow-right.svg" id="cta-arrow" alt="">
+                    </span>
+                </div>
+            </div>
+
+            <div class="service-card">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="images/yoga-pose.svg" alt="">
+                    </div>
+                    <span class="card-title">Lose Fat</span>
+                    <p class="card-desc">
+                        Learn how to gain strength while losing
+                        fat from our very experienced personal trainers.
+                    </p>
+                    <span class="learn-more-btn">
+                        Learn More
+                        <img src="images/arrow-right.svg" id="cta-arrow" alt="">
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section id="trainers">
+
+    <div class="trainers-wrapper">
+        <h1>Expert Trainers</h1>
+
+        <div class="trainers">
+            <div class="trainer-card">
+                <img src="images/trainer-1.jpg" alt="">
+                <div class="trainer-text">
+                    <span class="trainer-name">Eric Johnson</span>
+                    <span class="trainer-position">Strength Trainer</span>
+                </div>
+            </div>
+
+            <div class="trainer-card">
+                <img src="images/trainer-2.jpg" alt="">
+                <div class="trainer-text">
+                    <span class="trainer-name">Valerie Davidson</span>
+                    <span class="trainer-position">Yoga Instructor</span>
+                </div>
+            </div>
+
+            <div class="trainer-card">
+                <img src="images/trainer-3.jpg" alt="">
+                <div class="trainer-text">
+                    <span class="trainer-name">Luke Skywalker</span>
+                    <span class="trainer-position">Calisthenics Trainer</span>
+                </div>
+            </div>
+
+            <div class="trainer-card">
+                <img src="images/trainer-4.jpg" alt="">
+                <div class="trainer-text">
+                    <span class="trainer-name">Amanda Wilde</span>
+                    <span class="trainer-position">HIIT Specialist</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section id="items">
+    <div class="items-wrapper">
+        <div class="items-header-container">
+            <h1>Need Equipment? We Gotchu!</h1>
+            <button class="shop-btn">
+                See All
+            </button>
+        </div>
+
+        <div class="items">
+            <div class="item-card">
+                <div class="item-img">
+                    <img src="images/hero-item-1.png" alt="">
+                </div>
+
+                <div class="item-text">
+                    <p class="item-name">Jump Rope</p>
+                    <p class="item-price">&#36;9.99</p>
+                </div>
+            </div>
+
+            <div class="item-card">
+                <div class="item-img">
+                    <img src="images/hero-item-2.png" alt="">
+                </div>
+
+                <div class="item-text">
+                    <p class="item-name">Kettlebell</p>
+                    <p class="item-price">&#36;9.99</p>
+                </div>
+            </div>
+
+            <div class="item-card">
+                <div class="item-img">
+                    <img src="images/hero-item-3.png" alt="">
+                </div>
+
+                <div class="item-text">
+                    <p class="item-name">Dumbbells</p>
+                    <p class="item-price">&#36;9.99</p>
+                </div>
+            </div>
+
+            <div class="item-card">
+                <div class="item-img">
+                    <img src="images/hero-item-4.png" alt="">
+                </div>
+
+                <div class="item-text">
+                    <p class="item-name">Medicine Ball</p>
+                    <p class="item-price">&#36;9.99</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="blog-posts">
+    <div class="blog-wrapper">
+        <h1 class="blog-heading">From the Blog</h1>
+
+        <div class="blog-post-grid">
+            <div class="post-four">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/push-ups.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                        <p class="post-excerpt-long">
+                            Donec accumsan neque ac tellus consectetur, vitae
+                            faucibus eros fringilla. Duis justo justo, convallis
+                            nec enim a, sodales mattis diam. Aliquam blandit gravida
+                            lorem at iaculis. Donec id quam euismod, scelerisque est
+                            sit amet, ultricies ipsum...
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="post-two-1">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/muscle-ups.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                        <p class="post-excerpt-short">
+                            Donec accumsan neque ac tellus consectetur, vitae
+                            faucibus eros
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="post-two-2">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/leg-raises.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                        <p class="post-excerpt-short">
+                            Donec accumsan neque ac tellus consectetur, vitae
+                            faucibus eros
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="post-one-1">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/dumbbell-rows.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="post-one-2">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/split-squats.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="post-one-3">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/rings.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="post-one-4">
+                <div class="post-container">
+                    <div class="post-img">
+                        <img src="images/swings.jpg" alt="">
+                    </div>
+                    <div class="post-text">
+                        <h3 class="post-title">
+                            7 Reasons Why Pushups Are better Than Bench Press
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="testimonials">
+    <div class="testimonials-container">
+        <h1 class="testimonial-heading">
+            What Our Clients Say
+        </h1>
+
+        <div class="testimonials-flex">
+            <div class="testimonial-wrapper">
+                <div class="testimonial-img">
+                    <img src="images/testimonial-1.jpg" alt="">
+                </div>
+                <p class="testimonial">
+                    Donec accumsan neque ac tellus consectetur, vitae
+                    faucibus eros fringilla. Duis justo justo, convallis
+                </p>
+                <span class="testimonial-author">
+                    - Chad
+                </span>
+            </div>
+            <div class="testimonial-wrapper">
+                <p class="testimonial">
+                    Donec accumsan neque ac tellus consectetur, vitae
+                    faucibus eros fringilla. Duis justo justo, convallis
+                </p>
+                <span class="testimonial-author">
+                    - Stacy
+                </span>
+            </div>
+            <div class="testimonial-wrapper">
+                <div class="testimonial-img">
+                    <img src="images/testimonial-2.jpg" alt="">
+                </div>
+                <p class="testimonial">
+                    Donec accumsan neque ac tellus consectetur, vitae
+                    faucibus eros fringilla. Duis justo justo, convallis
+                </p>
+                <span class="testimonial-author">
+                    - Gwen
+                </span>
+            </div>
+        </div>
+
+        <button class="testimonials-btn">
+            More Success Stories
+        </button>
+    </div>
+</section>
+
+<footer>
+    <div class="footer-half">
+        <ul class="footer-links left-footer">
+            <li class="link-header">
+                Help
+            </li>
+            <li><a href="#">Returns</a></li>
+            <li><a href="#">Orders</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+
+        <ul class="footer-links right-footer">
+            <li class="link-header">
+                Account
+            </li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-half">
+        <ul class="footer-links left-footer">
+            <li class="link-header">
+                Pages
+            </li>
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Success Stories</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Sustainability</a></li>
+        </ul>
+
+        <ul class="footer-socials right-footer">
+            <li class="link-header">
+                Find Us On
+            </li>
+            <li>
+                <a href="#">
+                    <img src="images/facebook-icon.svg" alt="" title="facebook">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="images/youtube-icon.svg" alt="" title="youtube">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="images/instagram-icon.svg" alt="" title="instagram">
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+
+</html>
